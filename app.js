d60188f781c321c6e9a9f7a775e32a32738aac44
skmaurya08 <emailsshubham@gmail.com>
2024-11-15 13:14:43 +0530

    lastlastcommit

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ import cors from "cors";
 import dotenv from "dotenv";
 import "./db.js";
 dotenv.config();
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// };
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.get("/",(req,res)=>{
+  res.send("Live");
+})
 app.use(`/api`, userRoute);
 app.use(`/api/auth`, authRouter);
 app.use(`/api/test`, testRouter);
